dd23257732fc7db5c6cca05234ee5a190580b9e2
Lance J. <lancear153@gmail.com>
2023-12-04 20:18:52 +0100

    day 1 part 2

--- day-01/index.js
+++ day-01/index.js
@@ -1,6 +1,19 @@
 const process = require('process');
 const fs = require('fs');
 
+const spelledOutDigits = [
+  'zero',
+  'one',
+  'two',
+  'three',
+  'four',
+  'five',
+  'six',
+  'seven',
+  'eight',
+  'nine'
+];
+
 process.argv.shift(); // node
 process.argv.shift(); // index.js
 const inputFile = process.argv.shift();
@@ -9,8 +22,16 @@ const lines = fs.readFileSync(inputFile, "utf-8").split('\n');
 let sum = 0;
 
 for (const line of lines) {
-  const numbers = line.match(/[0-9]/g);
-  const value = numbers ? parseInt(`${numbers.at(0)}${numbers.at(-1)}`) : 0;
+  const numbers = Array
+    .from(
+      line.matchAll(/(?=([0-9]|one|two|three|four|five|six|seven|eight|nine))/g) ?? []
+    )
+    .map(([, str]) => {
+      const spelledOutValue = spelledOutDigits.findIndex(spelledOut => spelledOut === str);
+      return spelledOutValue !== -1 ? spelledOutValue : parseInt(str)
+    });
+
+  const value = numbers.length ? parseInt(`${numbers.at(0)}${numbers.at(-1)}`) : 0;
   sum += value;
 }
 
